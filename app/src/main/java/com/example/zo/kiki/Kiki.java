51d1730f7bf43df6b1da82b93baad78babd2ac61
Zoe <Zoe>
2017-02-25 22:12:06 +0100

    ajout d'images, ajout de l'écran de Kiki et lien entre les deux écrans.

--- app/src/main/java/com/example/zo/kiki/Kiki.java
+++ app/src/main/java/com/example/zo/kiki/Kiki.java
@@ -1,10 +1,25 @@
 package com.example.zo.kiki;
 
 import android.app.Activity;
-
-/**
- * Created by Zoé on 25/02/2017.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Kiki extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ecran_kiki);
+        Button ca_me_deplait = (Button)findViewById(R.id.ca_me_deplait);
+        ca_me_deplait.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Kiki", "Tant pis pour toi.");
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/zo/kiki/MainActivity.java
+++ app/src/main/java/com/example/zo/kiki/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.zo.kiki;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.ecran_accueil);
+        Button btn_cest_parti = (Button)findViewById(R.id.cest_Parti);
+        btn_cest_parti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Kiki", "C'est kiki !");
+                Intent intent = new Intent (MainActivity.this, Kiki.class);
+                startActivity(intent);
+            }
+        });
     }
 }
